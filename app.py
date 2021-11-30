ebe1fe5d3c943b0b8f7359292fc8f2f802a11094
gopaddle-demo <73924831+gopaddle-demo@users.noreply.github.com>
2021-11-30 18:37:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome!"
+    return "Hello World!"
 
-@app.route('/how are you')
+@app.route('/heartBeat')
 def hello():
-    return 'I am good, how about you?'
+    return 'Alive!'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
